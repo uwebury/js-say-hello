32e0bb3079b54e52499311875eec0cf7a9a0e563
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-20 12:36:53 +0100

    Entered excercise

---
+++ index.js
@@ -0,0 +1,14 @@
+// write a function called 'sayHello' that accepts two parameters: name and time.
+//
+// - if we pass no name, it should log "Good morning, Unknown"
+// - if we pass a name, it should log "Good morning, {name}";
+// - if time is greater than  12, it should log "Good afternoon, {name}"
+//
+function sayHello(name, time) {
+  if 
+}
+
+// test cases
+sayHello();
+sayHello("Jane");
+sayHello("Jane", 14);
