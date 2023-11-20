cad2ec31b5f96bbb50891e3a679fb9305d46e27a
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-20 13:05:34 +0100

    Added alternative solutions

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ function sayHello(name, time) {
   console.log("Good morning, ", name);
 }
 
+//alternative solution 1:
+//function sayHello(name, time) {
+//	if (!name) {
+//		name = "Unknown";
+//        // SUPER important: no return here!
+//        // because the code continues below, since we haven't logged anything yet
+//	}
+//	if (time > 12) {
+//		console.log("Good afternoon", name);
+//		return;
+//	}
+//	console.log("Good morning", name);
+//}
+
+//alternative solution 2:
+// with default parameters
+//function sayHello(name = 'Unknown', time) {
+//	if (time > 12) {
+//		console.log("Good afternoon", name);
+//		return;
+//	}
+//	console.log("Good morning", name);
+//}
+
 // test cases
 sayHello();
 sayHello("Jane");
